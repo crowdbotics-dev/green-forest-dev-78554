014bc466b9f0cb7534364ea2347b627c5f4195ad
Crowdbotics <engineering@crowdbotics.com>
2023-04-24 01:29:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "green-forest-dev-78554",
+  url: "https://green-forest-dev-78554.botics.co",
+  api: "https://green-forest-dev-78554.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
